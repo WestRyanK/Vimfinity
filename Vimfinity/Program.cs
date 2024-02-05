b419b16d645947cbad2df011db444c4a0c558225
Ryan West <west.ryan.k@gmail.com>
2024-02-05 15:53:20 -0700

    Translate vim keys unconditionally

--- Vimfinity/Program.cs
+++ Vimfinity/Program.cs
@@ -15,7 +15,21 @@ class Program
 
 	private static HookArgs Hook(HookArgs args)
 	{
-		Debug.WriteLine($"Key: {args.Key} State: {args.PressedState}");
+		//Debug.WriteLine($"Input Key: {args.Key} State: {args.PressedState}");
+		if (args.PressedState == KeyPressedState.Down && _InputKeysToOutput.TryGetValue(args.Key, out var output))
+		{
+			//Debug.WriteLine($"Output: {output}");
+			SendKeys.Send(output);
+			return HookArgs.Swallow;
+		}
 		return args;
 	}
+
+	private static readonly IDictionary<Keys, string> _InputKeysToOutput = new Dictionary<Keys, string>() {
+		{ Keys.H, "{Left}" },
+		{ Keys.J, "{Down}" },
+		{ Keys.K, "{Up}" },
+		{ Keys.L, "{Right}" },
+		{ Keys.X, "{Backspace}" },
+	};
 }
