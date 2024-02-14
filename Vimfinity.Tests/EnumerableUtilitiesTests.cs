a49566f3e1f0c40ff004539eeeb5f6c7cc6838bc
Ryan West <west.ryan.k@gmail.com>
2024-02-14 14:08:29 -0700

    Add AllEqual extension for enumerables

---
+++ Vimfinity.Tests/EnumerableUtilitiesTests.cs
@@ -0,0 +1,35 @@
+﻿namespace Vimfinity.Tests;
+public class EnumerableUtilitiesTests
+{
+	[Fact]
+	public void AllEqual_Null()
+	{
+		Assert.False(EnumerableUtilities.AllEqual<string?>(null, []));
+		Assert.False(EnumerableUtilities.AllEqual<string?>([], null));
+		Assert.True(EnumerableUtilities.AllEqual<string?>(null, null));
+	}
+
+	[Fact]
+	public void AllEqual_Equal()
+	{
+		Assert.True(EnumerableUtilities.AllEqual<string?>([], []));
+		Assert.True(EnumerableUtilities.AllEqual(["A"], ["A"]));
+		Assert.True(EnumerableUtilities.AllEqual(["A", "B", "C"], ["A", "B", "C"]));
+	}
+
+	[Fact]
+	public void AllEqual_NullElement()
+	{
+		Assert.True(EnumerableUtilities.AllEqual([null, "B"], [null, "B"]));
+		Assert.False(EnumerableUtilities.AllEqual([null, "B"], ["A", "B"]));
+		Assert.False(EnumerableUtilities.AllEqual(["A", "B", "C"], ["A", null, "C"]));
+	}
+
+	[Fact]
+	public void AllEqual_DifferentCounts()
+	{
+		Assert.False(EnumerableUtilities.AllEqual(["A"], []));
+		Assert.False(EnumerableUtilities.AllEqual(["A"], ["A", "B"]));
+		Assert.False(EnumerableUtilities.AllEqual(["A", "B", "C"], ["A", "B"]));
+	}
+}
---
+++ Vimfinity/Utilities/EnumerableUtilities.cs
@@ -0,0 +1,46 @@
+﻿namespace Vimfinity;
+
+internal static class EnumerableUtilities
+{
+	public static bool AllEqual<T>(this IEnumerable<T>? first, IEnumerable<T>? second)
+	{
+		if ((first == null) ^ (second == null))
+		{
+			return false;
+		}
+
+		if (first == null && second == null)
+		{
+			return true;
+		}
+
+		IEnumerator<T> enumerator1 = first!.GetEnumerator();
+		IEnumerator<T> enumerator2 = second!.GetEnumerator();
+
+		bool hasNext1, hasNext2;
+		while (
+			(hasNext1 = enumerator1.MoveNext()) &
+			(hasNext2 = enumerator2.MoveNext()))
+		{
+			T current1 = enumerator1.Current;
+			T current2 = enumerator2.Current;
+
+			if ((current1 == null) ^ (current2 == null))
+			{
+				return false;
+			}
+
+			if (current1 == null && current2 == null)
+			{
+				continue;
+			}
+
+			if (!current1!.Equals(current2))
+			{
+				return false;
+			}
+		}
+
+		return !hasNext1 && !hasNext2;
+	}
+}
